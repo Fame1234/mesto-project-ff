e84ea1d4040bce91a595b705c43cbfa107b7c0be
Nikolay <iceman17ea@gmail.com>
2025-01-10 20:07:57 +0300

    Исправление ошибок

--- src/index.js
+++ src/index.js
@@ -16,85 +16,67 @@ editButton.addEventListener('click', openModalEditProfile)
 formEditProfile.addEventListener('submit', handleFormEditSubmit)
 
 function openModalEditProfile() {
-	nameInput.value = profileTitle.textContent
-	jobInput.value = profileDescription.textContent
-	clearValidation(formEditProfile, validationConfig)
-	openModal(editPopup)
+    nameInput.value = profileTitle.textContent
+    jobInput.value = profileDescription.textContent
+    clearValidation(formEditProfile, validationConfig)
+    openModal(editPopup)
 }
 
 function handleFormEditSubmit(evt) {
-	evt.preventDefault()
-	const button = evt.target.querySelector('.popup__button')
-  renderLoading(true, button)
+    evt.preventDefault()
+    const button = evt.target.querySelector('.popup__button')
+    renderLoading(true, button)
 
-	const updateProfileData = {
-		name: nameInput.value,
-		about: jobInput.value,
-	}
-	editedUserData(updateProfileData)
-		.then(userData => {
-			renderUserData(userData)
-			closeModal(editPopup)
-		})
-		.catch(err => console.log(err))
-		.finally(() => renderLoading(false, button))
+    const updateProfileData = {
+        name: nameInput.value,
+        about: jobInput.value,
+    }
+    // Обновляем локально без использования сервера
+    renderUserData(updateProfileData)
+    closeModal(editPopup)
+    renderLoading(false, button)
 }
 
 const addCardPopup = document.querySelector('.popup_type_new-card')
 const addButton = document.querySelector('.profile__add-button')
 
 const formNewPlace = document.querySelector('form[name="new-place"]')
-const namePlaceInput = formNewPlace.querySelector(
-	'.popup__input_type_card-name'
-)
+const namePlaceInput = formNewPlace.querySelector('.popup__input_type_card-name')
 const linkInput = formNewPlace.querySelector('.popup__input_type_url')
 
 // слушатель кнопки открытия попапа для добавления новой карточки
 addButton.addEventListener('click', evt => {
-	evt.stopPropagation()
-	clearValidation(formNewPlace, validationConfig)
-	openModal(addCardPopup)
+    evt.stopPropagation()
+    clearValidation(formNewPlace, validationConfig)
+    openModal(addCardPopup)
 })
 
 formNewPlace.addEventListener('submit', evt => {
-	evt.stopPropagation()
-	handleNewCardSubmit(evt)
-	document.querySelector('.popup__button').disabled = true
-	clearValidation(formNewPlace, validationConfig)
+    evt.stopPropagation()
+    handleNewCardSubmit(evt)
+    document.querySelector('.popup__button').disabled = true
+    clearValidation(formNewPlace, validationConfig)
 })
 
 // функция добавления новой карточки в начало страницы
 function handleNewCardSubmit(evt) {
-	evt.preventDefault()
-	const button = evt.target.querySelector('.popup__button')
-  renderLoading(true, button)
+    evt.preventDefault()
+    const button = evt.target.querySelector('.popup__button')
+    renderLoading(true, button)
 
-	const namePlaceValue = namePlaceInput.value
-	const linkValue = linkInput.value
+    const namePlaceValue = namePlaceInput.value
+    const linkValue = linkInput.value
 
-	const newCardData = {
-		name: namePlaceValue,
-		link: linkValue,
-	}
-	addNewCard(newCardData)
-		.then(data => {
-			cardList.prepend(
-				addCard(
-					myId,
-					data,
-					cardTemplate,
-					openImgModal,
-					openModalDeleteCard,
-					handleLike
-				)
-			)
-			closeModal(addCardPopup)
-			formNewPlace.reset()
-		})
-		.catch(err => {
-			console.log(err)
-		})
-		.finally(() => renderLoading(false, button))
+    const newCardData = {
+        name: namePlaceValue,
+        link: linkValue,
+    }
+    // Добавляем карточку локально, без использования сервера
+    const newCardElement = addCard(newCardData)
+    cardList.prepend(newCardElement)
+    closeModal(addCardPopup)
+    formNewPlace.reset()
+    renderLoading(false, button)
 }
 
 // <----------------------------------------------------------->
@@ -106,15 +88,14 @@ const modalCaption = modalTypeImg.querySelector('.popup__caption')
 
 // функция открытия модального окна изображения карточки
 function openImgModal(img) {
-	modalImg.src = img.src
-	modalImg.alt = img.alt
-	modalCaption.textContent = img.alt
-	openModal(modalTypeImg)
+    modalImg.src = img.src
+    modalImg.alt = img.alt
+    modalCaption.textContent = img.alt
+    openModal(modalTypeImg)
 }
 
 // <---------------------------------------------------------->
 
-// переменные для попапа удаления карточки по корзине
 const deleteCardPopup = document.querySelector('.popup_type_delete-card')
 let cardDelete
 let cardElementDelete
@@ -122,47 +103,47 @@ const deleteCardButton = document.querySelector('.popup__button_delete-card')
 
 // функция открытия попапа для удаления карточки
 function openModalDeleteCard(card, cardElement) {
-	cardDelete = card
-	cardElementDelete = cardElement
-	openModal(deleteCardPopup)
+    cardDelete = card
+    cardElementDelete = cardElement
+    openModal(deleteCardPopup)
 }
 
 // функция обработчик удаления карточки
 function handleDeleteCard(evt, card, cardElement) {
-	evt.preventDefault()
-	deleteCard(card)
-		.then(() => cardElement.remove())
-		.catch(err => console.log(err))
+    evt.preventDefault()
+    // Удаляем карточку локально
+    cardElement.remove()
+    closeModal(deleteCardPopup)
 }
 
 // удаление карточки по кнопке согласия "да"
 deleteCardButton.addEventListener('click', evt => {
-	evt.stopPropagation()
-	handleDeleteCard(evt, cardDelete, cardElementDelete)
-	closeModal(deleteCardPopup)
+    evt.stopPropagation()
+    handleDeleteCard(evt, cardDelete, cardElementDelete)
 })
 
 // функция для постановки и снятия лайка
 function handleLike(button, cardId, countLikes) {
-	const likeMethod = button.classList.contains('card__like-button_is-active')
-		? deleteLikeCard
-		: likeCard
-	likeMethod(cardId)
-		.then(cardData => {
-			countLikes(cardData.likes)
-			button.classList.toggle('card__like-button_is-active')
-		})
-		.catch(err => console.log(err))
+    const likeMethod = button.classList.contains('card__like-button_is-active')
+        ? removeLike
+        : addLike
+    likeMethod(cardId)
+        .then(cardData => {
+            countLikes(cardData.likes)
+            button.classList.toggle('card__like-button_is-active')
+        })
+        .catch(err => console.log(err))
 }
 
 // <--------------------------------------------------->
+
 // ЗАКРЫТИЕ ПОПАПОВ ПО КРЕСТИКУ
 // все кнопки закрытия попапов
 const closeButtons = document.querySelectorAll('.popup__close')
 
 closeButtons.forEach(button => {
-	button.addEventListener('click', evt => {
-		const popup = evt.target.closest('.popup')
-		closeModal(popup)
-	})
+    button.addEventListener('click', evt => {
+        const popup = evt.target.closest('.popup')
+        closeModal(popup)
+    })
 })
